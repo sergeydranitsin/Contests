9f9a232f85dd5d4c02a4ded2c219733d41239d28
ivanp <ivanpolkin@yandex.ru>
2018-05-01 21:16:32 +0500

    changed callback urls

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'vkontakte' => [
         'client_id' => '6463721',
         'client_secret' => 'AKkjcXFy5LgFIsIEENDs',
-        'redirect' => 'http://localhost/login/vk/callback',
+        'redirect' => env('APP_URL') . '/login/vk/callback',
 //        'client_id' => env('VKONTAKTE_KEY'),
 //        'client_secret' => env('VKONTAKTE_SECRET'),
 //        'redirect' => env('VKONTAKTE_REDIRECT_URI'),
@@ -48,7 +48,7 @@ return [
         'client_id' => '1266105344',
         'client_public' => 'CBANDIHMEBABABABA',
         'client_secret' => '4DE10C04C011F68D0D303A80',
-        'redirect' => 'http://localhost/login/ok/callback',
+        'redirect' => env('APP_URL') . '/login/ok/callback',
 //        'client_id' => env('ODNOKLASSNIKI_ID'),
 //        'client_secret' => env('ODNOKLASSNIKI_SECRET'),
 //        'client_public' => env('ODNOKLASSNIKI_PUBLIC'),
